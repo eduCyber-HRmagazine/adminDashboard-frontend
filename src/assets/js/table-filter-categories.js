6c2bc40854fed47bd99aa16c3f174b3b9a88d208
Shimaa-Mahmoud-Abdelazeem <142457787+Shimaa-Mahmoud-Abdelazeem@users.noreply.github.com>
2024-04-01 03:01:09 +0200

    add table-filter-categories.js

---
+++ src/assets/js/table-filter-categories.js
@@ -0,0 +1,29 @@
+// input element 
+document.getElementById('title').addEventListener('input', function() {
+    let filterValue = this.value.trim().toLowerCase();
+    //  all rows in the table
+    let categories = document.querySelectorAll('#dataTable tr');
+
+    // Loop through each row
+    categories.forEach(function(category) {
+        let categoryName = category.querySelector('td:first-child input').value.toLowerCase();
+        
+        if(categoryName.includes(filterValue)) {
+            category.style.display = 'table-row';
+        } else {
+            category.style.display = 'none';
+        }
+    });
+});
+
+document.getElementById('showAllTable').addEventListener('click', function() {
+    let categories = document.querySelectorAll('#dataTable tr');
+    // display all rows
+    categories.forEach(function(category) {
+        category.style.display = 'table-row';
+    });
+});
+    
+
+
+
--- src/assets/js/table-filter-tag.js
+++ src/assets/js/table-filter-tag.js
@@ -1,22 +1,31 @@
-// Get the input element where the user will type the filter keyword
+// input element 
 const filterInput = document.getElementById('title');
-
-// Get all the tag elements
+// all the tags
 const tagElements = document.querySelectorAll('.custom-input');
+
+const showAllTable = document.getElementById('showAllTable');
 
 // Add input event listener to the filter input
 filterInput.addEventListener('input', function() {
-    const filterValue = this.value.toLowerCase(); // Get the value entered by the user and convert it to lowercase
+    const filterValue = this.value.toLowerCase(); 
 
-    // Loop through each tag element and check if it matches the filter value
+    // Loop each tag and check if it matches the filter value
     tagElements.forEach(function(tagElement) {
         const tagText = tagElement.value.toLowerCase();
         const parentTr = tagElement.closest('tr');
 
         if (tagText.includes(filterValue)) {
-            parentTr.style.display = ''; // Show the tag if it matches the filter value
+            parentTr.style.display = ''; 
         } else {
-            parentTr.style.display = 'none'; // Hide the tag if it does not match the filter value
+            parentTr.style.display = 'none'; 
         }
     });
 });
+
+showAllTable.addEventListener('click', function() {
+    customInputs.forEach(function(customInput) {
+        const parentRow = customInput.closest('tr');
+        parentRow.style.display = '';
+    });
+});
+
